d30387b6454705dca52df43b3b8d4e8bb848a36a
ivan <mytsakivan@gmail.com>
2019-04-22 01:44:51 +0300

    remove method

--- index.js
+++ index.js
@@ -8,19 +8,14 @@ ShopCart.prototype = new Cart;
 
 
 // ...your code there
-ShopCart.prototype.remove = function(el) {
-    const cart = new ShopCart();
-    if(typeof el === 'object' && !Array.isArray(el)){
-      cart.items.forEach(function(curVal,index){
-      		if(curVal.title===el.title && curVal.id===el.id){
-      			var res = cart.items.splice(index,1);
-      			return cart.items;
-      		}else{return cart.items;}
-      });
-    	
-    }else{console.log("Sorry but we can't find this item");}
 
-}
+Cart.prototype.remove = function (item) {
+     const index = this.getItemIndex(item);
+    if (index !== null) {
+       var res = this.items.splice(index,1);
+    }
+};
+
 
 
 module.exports = ShopCart;
