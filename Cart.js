913632211ea20704a58ffdb6d549569878130fa0
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-04-21 21:04:38 +0300

    methods for Shop Cart

--- Cart.js
+++ Cart.js
@@ -1,6 +1,7 @@
 function Cart() {
     this.items = [];
 }
+
 Cart.prototype.add = function (item, count = 1) {
     const index = this.getItemIndex(item);
     if (index === null) {
@@ -14,22 +15,25 @@ Cart.prototype.add = function (item, count = 1) {
             count: this.items[index].count + count
         };
     }
-}
+};
+
 Cart.prototype.remove = function (item) {
     return null;
-}
+};
+
 Cart.prototype.clear = function () {
     return null;
-}
+};
+
 Cart.prototype.setCount = function () {
     return null;
-}
+};
+
 Cart.prototype.getItemIndex = function(item) {
     return this.items.reduce((i, el, index) => {
         if (el.item.id === item.id) i = index;
         return i;
     }, null);
-}
-
+};
 
 module.exports = Cart;
--- index.js
+++ index.js
@@ -5,6 +5,38 @@ function ShopCart() {
 }
 ShopCart.prototype = new Cart;
 
-// ...your code there
+ShopCart.prototype.remove = function(item){
+    const index = this.getItemIndex(item);
+
+    this.items.splice(index, 1);
+};
+
+ShopCart.prototype.clear = function(){
+    return this.items = [];
+};
+
+ShopCart.prototype.setCount = function(item, count){
+    const index = this.getItemIndex(item);
+
+    if(count <= 0 ) {
+        throw Error('Count should be greater than zero')
+    } else {
+        this.items[index] = {
+            item: item,
+            count: this.items[index].count = count
+        };
+    }
+};
+
+ShopCart.prototype.toString = function (){
+    const arr = [0];
+    const sum = (accumulator, currentValue) => accumulator + currentValue;
+
+    for(let i = 0; i < this.items.length; i++){
+        arr.push(this.items[i].count);
+    }
+
+    return `In your cart ${ arr.reduce(sum) } items`
+};
 
 module.exports = ShopCart;
