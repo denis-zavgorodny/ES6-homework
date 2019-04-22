0b7e8cdb86cabc6e4e3886a09993ca3b420172a7
ivan mytsak <mytsakivan@gmail.com>
2019-04-22 13:53:35 +0300

    and last method .toString()

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ ShopCart.prototype = new Cart;
 
 
 // ...your code there
-Cart.prototype.remove = function (item) {
-     const index = this.getItemIndex(item);
+Cart.prototype.remove = function(item) {
+    const index = this.getItemIndex(item);
     if (index !== null) {
-       var res = this.items.splice(index,1);
+        var res = this.items.splice(index, 1);
     }
 };
 
-Cart.prototype.setCount = function (item, count) {
-    if(count<=0)
-    	throw("Count should be greater than zero");
-    
+Cart.prototype.setCount = function(item, count) {
+    if (count <= 0)
+        throw ("Count should be greater than zero");
+
     const index = this.getItemIndex(item);
     if (index === null) {
         this.items.push({
@@ -28,28 +28,30 @@ Cart.prototype.setCount = function (item, count) {
     } else {
         this.items[index] = {
             item: item,
-            count:count
+            count: count
         };
     }
 };
 
-Cart.prototype.clear = function(){
-	if(this.items.length>0){
-		this.items.splice(this.items.length);
-	}
+Cart.prototype.clear = function() {
+    if (this.items.length > 0) {
+        this.items.splice(this.items.length);
+    }
 };
 
+Cart.prototype.objToArr = function(obj) {
+    Object.keys(obj).map(function(key) {
+        console.log([Number(key), obj[key]]);
+    });
+}
 
-Cart.prototype.toString = function(){
-	let _items;
-	this.items.forEach(function(curVal,index){
-		 _items += this.items[index].count;
-	});
-	console.log(this.items);
-	
+Cart.prototype.toString = function() {
+    let cnt = 0;
+    this.items.map(function(el) {
+        cnt += Number.parseInt(el.count);
+    });
+    return `In your cart ${cnt} items`;
 };
-
-
 
 
 module.exports = ShopCart;
