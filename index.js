c6a2e114990f098a85cbbecb37aeef7a570f1532
ivan mytsak <mytsakivan@gmail.com>
2019-04-22 13:07:42 +0300

    count item should be greater than 0

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ ShopCart.prototype = new Cart;
 
 
 // ...your code there
-
 Cart.prototype.remove = function (item) {
      const index = this.getItemIndex(item);
     if (index !== null) {
@@ -17,6 +16,9 @@ Cart.prototype.remove = function (item) {
 };
 
 Cart.prototype.setCount = function (item, count) {
+    if(count<=0)
+    	throw("Count should be greater than zero");
+    
     const index = this.getItemIndex(item);
     if (index === null) {
         this.items.push({
@@ -31,6 +33,21 @@ Cart.prototype.setCount = function (item, count) {
     }
 };
 
+Cart.prototype.clear = function(){
+	if(this.items.length>0){
+		this.items.splice(this.items.length);
+	}
+};
+
+
+Cart.prototype.toString = function(){
+	let _items;
+	this.items.forEach(function(curVal,index){
+		 _items += this.items[index].count;
+	});
+	console.log(this.items);
+	
+};
 
 
 
