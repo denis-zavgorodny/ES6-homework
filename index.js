3e06af7c766043e8ab477fe4ca9eef9367feb519
Natalya Thoryk <t.natalya@improveit.solutions>
2019-04-23 10:55:41 +0300

    ES6 homework Natasha Thoryk Add methods: add, remove, clear, setCount to ShopCart prototype

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ function ShopCart() {
 }
 ShopCart.prototype = new Cart;
 
-// ...your code there
+ShopCart.prototype.remove = function (item) {
+    const index = this.getItemIndex(item);
+    if (index !== null) {
+        this.items.splice(index, 1)
+    }
+};
+
+ShopCart.prototype.clear = function () {
+    this.items = [];
+};
+
+ShopCart.prototype.setCount = function (item, count) {
+    const index = this.getItemIndex(item);
+    if (count > 0) {
+        this.items[index] = {
+            item: item,
+            count: count
+        };
+    } else {
+        throw 'Count should be greater than zero'
+    }
+};
+
+ShopCart.prototype.toString = function () {
+    let result = 0;
+    this.items.forEach(item => {
+        let count = item.count;
+        result = result + count;
+    });
+    return `In your cart ${result} items`
+};
 
 module.exports = ShopCart;
